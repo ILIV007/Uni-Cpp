d6f2246ec820e564f1677bea75aade5aad99ba04
ILIYA <119437321+ILIV007@users.noreply.github.com>
2026-05-27 19:01:42 +0330

    Add files via upload

---
+++ phone.file/phone_file.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+int main()
+{
+    /*
+    ofstream phone_file("phone.txt");
+    long int number;
+    string name;
+    cout<<"enter number and name (0 for exit)\n";
+    for(;;)
+    {
+        cin>>number;
+        if(number==0)
+         break;
+        phone_file<<number<<"\t";
+
+        cin>>name;
+        phone_file<<name<<endl;
+        cout << endl;
+    }
+    */
+   //--------------------------------------------find:
+   ifstream phone_file("phone.txt",ios::in);
+   long int number;
+   string name , find_name;
+   bool find = false;
+   cin>>find_name;
+   cout<<endl;
+   while(cin>>find)
+   {
+    phone_file >> name;
+    if (name== find_name)
+    {
+        cout<<name<<' '<<number;
+        find=true;
+        break;
+    }
+   }
+   if(find==false)
+    cout<< "not found!!";
+    
+    return 0;
+}
